fcaa17a35d893e0b00a46c44ae5c203843d0f326
Priya Choudhary <priya@tothenew.com>
2016-04-27 15:45:03 +0530

    ECMAScript 6

---
+++ demo.js
@@ -0,0 +1,5 @@
+/**
+ * Created by priya on 27/4/16.
+ */
+export function sum (x, y) { return x + y };
+export var pi = 3.141593;
---
+++ demo1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by priya on 27/4/16.
+ */
+'use strict'
+
+import {sum, pi} from "./demo.js";
+console.log("2π = " + sum(pi,pi));
+
